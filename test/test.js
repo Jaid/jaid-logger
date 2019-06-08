b183fe7e1f0bf1972e85be0029ddcf8298b08505
Jaid <jaid.jsx@gmail.com>
2019-06-08 03:43:30 +0200

    Bumped package version (minor)

--- test/test.js
+++ test/test.js
@@ -3,13 +3,14 @@ import fs from "fs"
 
 import appdataPath from "appdata-path"
 import moment from "moment"
+import filenamify from "filenamify"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 const {default: jaidLogger} = indexModule
 
 it("should run", () => {
   const dateString = moment().format("YYYY-MM-DD")
-  const id = `${_PKG_NAME}-test`
+  const id = `${_PKG_NAME}-test` |> filenamify
   const logger = jaidLogger(id)
   expect(logger.appFolder.endsWith(id)).toBeTruthy()
   expect(logger.logFolder.endsWith("log")).toBeTruthy()
