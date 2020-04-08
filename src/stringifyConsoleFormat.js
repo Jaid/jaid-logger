0d19bef829eb4ec404c16d2c9fec907f06cec515
Jaid <jaid.jsx@gmail.com>
2020-04-08 14:29:43 +0200

    fix: Fixed NodeJS global shadowing
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/stringifyConsoleFormat.js
+++ src/stringifyConsoleFormat.js
@@ -4,7 +4,7 @@ import cleanStack from "clean-stack"
 import purdy from "purdy"
 import {isObject} from "lodash"
 
-const process = value => {
+const formatValue = value => {
   if (value instanceof Error) {
     if (value.stack) {
       return value.stack
@@ -26,7 +26,7 @@ export default format(info => {
   if (!splat || !splat.length) {
     return info
   }
-  info.splat = splat.map(x => process(x))
+  info.splat = splat.map(x => formatValue(x))
   delete info[SPLAT]
   return info
 })
